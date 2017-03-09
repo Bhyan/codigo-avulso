6a666640234d070248bcb11f8e3eeac33a9d1d23
Bryan Brito <bhyanbrito+github@gmail.com>
2017-03-09 04:29:45 -0300

    Adicionado sudo

--- programas_arch.sh
+++ programas_arch.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 
-#Script de desinstalação de programas que não utilizo no gnome e instalação de programas que utilizo 
-#habitualmente no arch linux.
-
-#O script e para ser usado no primeiro login, ou seja, com o usuário root, caso queira usar como usuário normal
-#depois de configurar o sudo, esse comando deve ser adicionado antes dos comandos.
-
+# Script de desinstalação de programas que não utilizo no gnome e instalação de programas que utilizo 
+# habitualmente no arch linux.
 
 # Programas que já vem no gnome.
-pacman -Rscn gnome-contacts && \
-pacman -Rscn gnome-dictionary && \
-pacman -Rscn dconf-editor && \
-pacman -Rscn empathy && \
-pacman -Rscn totem && \
-pacman -Rscn eog && \
-pacman -Rscn xterm && \
-pacman -Rscn epiphany && \
+sudo pacman -Rscn gnome-contacts && \
+sudo pacman -Rscn gnome-dictionary && \
+sudo pacman -Rscn dconf-editor && \
+sudo pacman -Rscn empathy && \
+sudo pacman -Rscn totem && \
+sudo pacman -Rscn eog && \
+sudo pacman -Rscn xterm && \
+sudo pacman -Rscn epiphany && \
+sudo pacman -Rscn tracker && \
 
-#Programas que serão instalados.
-pacman -S libreoffice libreoffice-pt-BR && \ #Pacote de edição de texto.
-pacman -S gnome-boxes && \ #Gerenciador de máquina virtual.
-pacman -S firefox && \ #Navegador de internet.
-pacman -S arandr && \ #Gerenciador de HDMI, VGA ...
-pacman -S qbittorrent && \ #Client torrent.
-pacman -S gedit && \ #Editor de texto, estilo notpad.
-pacman -S thunderbird && \ #E-mail.
-pacman -S chromium && \ #Navegador, só para netflix xD.
-pacman -S gimp && \ #Editor de imagem.
-pacman -S keepass && \ #Gerenciador de senha.
-pacman -S klavaro && \ #Programa para treinar digitação corretamente.
-pacman -S vlc && \ #visualizador de vídeo.
-pacman -S gpicview && \ #Visualizador de imagem.
-pacman -Syu #Atualização do sistema.
+# Programas que serão instalados.
+sudo pacman -S libreoffice libreoffice-pt-BR && \ # Pacote de edição de texto.
+sudo pacman -S gnome-boxes && \ # Gerenciador de máquina virtual.
+sudo pacman -S firefox && \ # Navegador de internet.
+sudo pacman -S arandr && \ # Gerenciador de HDMI, VGA ...
+sudo pacman -S qbittorrent && \ # Client torrent.
+sudo pacman -S thunderbird && \ # E-mail.
+sudo pacman -S chromium && \ # Navegador, só para netflix xD.
+sudo pacman -S gimp && \ # Editor de imagem.
+sudo pacman -S keepass && \ # Gerenciador de senha.
+sudo pacman -S klavaro && \ # Programa para treinar digitação corretamente.
+sudo pacman -S vlc && \ # visualizador de vídeo.
+sudo pacman -S gpicview && \ # Visualizador de imagem.
+sudo pacman -S git && \
+sudo pacman -Syu # Atualização do sistema.
