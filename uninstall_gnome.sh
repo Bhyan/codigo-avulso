3d3334a2d27b5378a53ebd9960d59ffffa27b8f4
Bryan Brito <bhyanbrito+github@gmail.com>
2019-11-30 18:03:15 -0300

    Add files via upload

---
+++ uninstall_gnome.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+pacman -Rscn gnome-boxes \
+	gnome-builder \
+	gnome-calendar \
+	five-or-more \
+	gnome-contacts \
+	pavucontrol \
+	devhelp \
+	simple-scan \
+	gnome-documents \
+	evolution \
+	gnome-nettool \
+	gnome-photos \
+	libgda lightsoff \
+	gnome-klotski \
+	iagno \
+	hitori \
+	glade \
+	ghex \
+	libreoffice-fresh \
+	gnome-books \
+	gnome-mahjongg \
+	gnome-maps \
+	gnome-weather \
+	gnome-mines \
+	gnome-music \
+	gnome-nibbles \
+	polari \
+	quadrapassel \
+	four-in-a-row \
+	gnome-recipes \
+	gnome-clocks \
+	gnome-robots \
+	gnome-sudoku \
+	swell-foop \
+	sysprof \
+	tali \
+	gnome-taquin \
+	gnome-todo \
+	terminator \
+	gnome-tetravex \
+	xterm \
+	epiphany \
+	gnome-chess \
+	totem \
+	gnome-sound-recorder \
+	gnome-multi-writer \
+	accerciser \
+	gnome-usage \
+	gnome-software \
+	yelp \
+	gnome-dictionary \
+	gnome-characters \
+	gnome-font-viewer 
+
+
+rm -r /usr/bin/gpk-update-viewer
+rm -r /usr/bin/gpk-application
