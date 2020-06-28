3cca8dfc6ea38d9c6bf6cdf5d1d9abc74a3a32be
Bryan Brito <bhyanbrito+github@gmail.com>
2020-06-28 15:07:50 -0300

    Update uninstall_cinnamon.sh

--- uninstall_cinnamon.sh
+++ uninstall_cinnamon.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-pacman -Rscn libreoffice-fresh \
+sudo chsh --shell /bin/bash bryan
+
+sudo pacman -Rscn libreoffice-fresh \
 	     libgda \
 	     terminator \
 	     xterm
+
+git clone https://aur.archlinux.org/package-query.git
+git clone https://aur.archlinux.org/yaourt.git
+
+cd package-query && makepkg -si
+
+cd ../yaourt && makepkg -si
+
+yaourt -S spotify
+# Erro de chave -> curl -sS https://download.spotify.com/debian/pubkey.gpg | gpg --import -
+
+yaourt -S dropbox
+# Erro de chave -> gpg --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
+
+yaourt -S slack-desktop
+
+sudo pacman -S git \
+			   maven \
+			   gnome-calculator \
+			   evince \
+			   keepass \
+			   pgadmin4 \
+			   jdk11-openjdk \
+
+sudo echo 'JAVA_HOME = "/usr/lib/jvm/java-11-openjdk/"' >> /etc/environment 
